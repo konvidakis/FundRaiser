88c0fbe985825c4796a28b4834bbd1502f673539
MikePante <mikepante@hotmail.com>
2021-05-23 13:33:53 +0300

    Transaction

---
+++ FundRaiser/Model/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser.Model
+{
+    class Transaction
+
+    {
+        public class Transaction
+        {
+            public int RewardId { get; set; }
+            public int UserId { get; set; }
+            public int ProjectId { get; set; }
+
+            public decimal Amount { get; set; }
+            public DateTime TimeStamp { get; set; }
+
+
+        }
+    }
+}
+}
