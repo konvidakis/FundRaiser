81f6f76d791abe03acb18935fac3bd9686f79b35
Vasilis Vrachoritis <vvrachoritis@athtech.gr>
2021-05-23 13:28:26 +0300

    Added the Project entity class to the model and the enum type Category file

---
+++ FundRaiser/Model/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser.Model
+{
+    class Project
+    {
+        public int ProjectId { get; set; }
+        public Category Category { get; set; }
+        public string Title { get; set; }
+        public string Logo { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Description { get; set; }
+        public decimal SetGoal { get; set; }
+        public decimal AmountPledged { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public User User { get; set; }
+    }
+}
