402b2b9563e8deb1b5c1ba15e5619a3b3a9f721b
Giannis-M <ioannis.mitsakos92@gmail.com>
2021-05-23 13:23:50 +0300

    user/projectpost

---
+++ FundRaiser/Model/ProjectPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser.Model
+{
+    public class ProjectPost
+    {
+        public  string Text { get; set; }
+        public string photo { get; set; }
+        public string Type { get; set; }
+        public DateTime TimeStamps { get; set; }
+        public List<ProjectId> projectIds { get; set; }
+    }
+}
