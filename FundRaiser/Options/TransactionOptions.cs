81a51d256b27d51517a3386d310a7cdac8451dc6
Mixalis Pantelakis <mpantelakis@athtech.gr>
2021-05-25 18:04:35 +0300

    .

---
+++ FundRaiser/Options/TransactionOptions.cs
@@ -0,0 +1,21 @@
+﻿using FundRaiser.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser.Options
+{
+    public class TransactionOption
+    {
+        public int TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Reward Reward { get; set; }
+        public User User { get; set; }
+        public Project Project { get; set; }
+
+    }
+
+}
