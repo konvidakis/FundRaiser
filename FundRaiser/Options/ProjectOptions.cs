9f64a732c1f17b728a9522eea63d86f487cae5e7
Vasilis Vrachoritis <vvrachoritis@athtech.gr>
2021-05-25 18:11:59 +0300

    Added ProjectOptions

---
+++ FundRaiser/Options/ProjectOptions.cs
@@ -0,0 +1,27 @@
+﻿using FundRaiser.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundRaiser.Options
+{
+    public class ProjectOptions
+    {
+        public int ProjectId { get; set; }
+        public Category Category { get; set; }
+        public string Title { get; set; }
+        public string Logo { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Description { get; set; }
+        public decimal SetGoal { get; set; }
+        public decimal AmountPledged { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string UserId { get; set; }
+        //public List<Transaction> Transactions { get; set; }
+        //public List<Reward> Rewards { get; set; }
+        //public List<ProjectPost> ProjectPosts { get; set; }
+    }
+}
