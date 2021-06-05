9cc9766a51d81fe40371b08b661591817c439539
Konstantinos Vidakis <vidakis@unipi.gr>
2021-06-05 23:59:05 +0300

    add Fund Project functionality

--- FundRaiser.Web/Controllers/ProjectPageController.cs
+++ FundRaiser.Web/Controllers/ProjectPageController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FundRaiser.Options;
 using FundRaiser.Service;
 using FundRaiser.Web.Models;
 
@@ -13,12 +14,14 @@ namespace FundRaiser.Web.Controllers
         private readonly IProjectService _projectService;
         private readonly IRewardService _rewardService;
         private readonly IProjectPostService _projectPostService;
+        private readonly ITransactionService _transactionService;
 
-        public ProjectPageController(IProjectService projectService, IRewardService rewardService, IProjectPostService projectPostService)
+        public ProjectPageController(IProjectService projectService, IRewardService rewardService, IProjectPostService projectPostService, ITransactionService transactionService)
         {
             _projectService = projectService;
             _projectPostService = projectPostService;
             _rewardService = rewardService;
+            _transactionService = transactionService;
         }
         public IActionResult Details(int id)
         {
@@ -34,6 +37,21 @@ namespace FundRaiser.Web.Controllers
             projectPage.ProjectPostOptions = _projectPostService.GetAllProjectPosts(id);
 
             return View(projectPage);
+        }
+
+        public IActionResult FundProject(int rewardId, int projectId, decimal amount, int userId)
+        {
+            RewardOption rewardOption = _rewardService.GetRewardById(rewardId);
+            TransactionOption transactionOption = new TransactionOption()
+            {
+                Amount = rewardOption.AmountRequired,
+                RewardId = rewardId,
+                ProjectId = projectId,
+                UserId = userId
+            };
+            _transactionService.CreateTransaction(transactionOption);
+
+            return RedirectToAction("Index", "Projects");
         }
     }
 }
