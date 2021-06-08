53ebe1879151725d0e299c53992ceb52a55d21d5
Konstantinos Vidakis <vidakis@unipi.gr>
2021-06-08 18:00:19 +0300

    create new View Model for Project with Progress

---
+++ FundRaiser.Web/Models/ProjectWithProgressViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FundRaiser.Options;
+
+namespace FundRaiser.Web.Models
+{
+    public class ProjectWithProgressViewModel
+    {
+        public ProjectOption ProjectOption { get; set; }
+        public decimal FinancialProgress { get; set; }
+    }
+}
